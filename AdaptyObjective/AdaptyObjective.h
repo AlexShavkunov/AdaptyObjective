a737921ae1df2ac0fdd539444b5b3c718d08b981
Alex Shavkunov <aleeexshavkunov@gmail.com>
2024-07-12 22:34:58 +0500

    Initial Commit

---
+++ AdaptyObjective/AdaptyObjective.h
@@ -0,0 +1,18 @@
+//
+//  AdaptyObjective.h
+//  AdaptyObjective
+//
+//  Created by Alex Shavkunov on 12.07.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdaptyObjective.
+FOUNDATION_EXPORT double AdaptyObjectiveVersionNumber;
+
+//! Project version string for AdaptyObjective.
+FOUNDATION_EXPORT const unsigned char AdaptyObjectiveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdaptyObjective/PublicHeader.h>
+
+
